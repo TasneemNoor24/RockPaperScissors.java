e165cab7b54174e3b208de7a529d02c7a8315d76
Tasneem Noor <tasneemnoor93@gmail.com>
2026-06-06 21:06:02 +0500

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"Rock", "Paper", "Scissors"};
+
+        System.out.println("=== Rock Paper Scissors Game ===");
+        System.out.print("Enter Rock, Paper, or Scissors: ");
+
+        String userChoice = sc.nextLine();
+
+        int computerIndex = random.nextInt(3);
+        String computerChoice = choices[computerIndex];
+
+        System.out.println("Computer chose: " + computerChoice);
+
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+            System.out.println("It's a Tie!");
+        }
+        else if ((userChoice.equalsIgnoreCase("Rock") && computerChoice.equals("Scissors")) ||
+                 (userChoice.equalsIgnoreCase("Paper") && computerChoice.equals("Rock")) ||
+                 (userChoice.equalsIgnoreCase("Scissors") && computerChoice.equals("Paper"))) {
+            System.out.println("You Win!");
+        }
+        else {
+            System.out.println("Computer Wins!");
+        }
+
+        sc.close();
+    }
+}
